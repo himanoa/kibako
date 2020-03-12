76afec8fca02cd1ac3818c92a63c41cbcb3dc98e
himanoa <matsunoappy@gmail.com>
2020-03-12 13:28:28 +0900

    feat: implemented container class

--- packages/core/src/index.test.ts
+++ packages/core/src/index.test.ts
@@ -1,5 +1,25 @@
-describe("test", () => {
-  it("should be 2", () => {
-    expect(1 + 1).toBe(2)
+import Container from "./index"
+
+const targetFn = (deps: {dep1: string}) => () => 2
+const container  = new Container()
+
+container.bind("foo", () => "foo")
+container.bind(targetFn, (container) => targetFn(container.resolve("foo")))
+
+describe("Container", () => {
+  describe("#resolve", () => {
+    describe("when dependencies can not be resolved", () => {
+      it("should be resoled", () => {
+        expect(() => { container.resolve("doo") }).toThrowError(
+          "doo is not defined"
+        )
+      })
+    })
+    describe("when dependencies can be resolved", () => {
+      it("should be resoled", () => {
+        const target = container.resolve(targetFn)
+        expect(target()).toBe(2)
+      })
+    })
   })
 })
---
+++ packages/core/src/index.ts
@@ -0,0 +1,19 @@
+type Resolver<T extends {(d: D): T}, D> =  (container: Container) => ReturnType<T>
+type Class<T> = T extends (null | undefined) ? never : { new(): T }
+type ID<T> = Required<Class<T>> | T
+
+export default class Container {
+  private dependenciesMap = new Map()
+  constructor() {}
+  bind<T extends {(deps: object): ReturnType<T>}>(key: ID<T>, resolver: Resolver<T, ReturnType<T>>) {
+    this.dependenciesMap.set(key, resolver)
+  }
+  resolve<T extends { (): ReturnType<T> }>(key: ID<T>): ReturnType<T> {
+    const resolver = this.dependenciesMap.get(key)
+    if(resolver) {
+      return resolver(this)
+    }
+    throw new Error(`${key} is not defined`)
+  }
+}
+
