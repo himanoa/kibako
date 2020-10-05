75bfed0d313872572d835a19b364895382f88927
himanoa <matsunoappy@gmail.com>
2020-10-05 12:11:29 +0900

    Fix test

--- packages/core/src/index.test.ts
+++ packages/core/src/index.test.ts
@@ -1,30 +1,32 @@
-import Container from "./index"
+import Container from "./index";
 
-const targetFn = (deps: {dep1: string}) => () => deps.dep1
-const container  = new Container()
+const targetFn = (deps: { dep1: string }) => () => deps.dep1;
+const container = new Container();
 
-container.bind("foo", () => "foo")
-container.bind(targetFn, (container) => targetFn({dep1: container.resolve("foo")}))
+container.bind("foo", () => "foo");
+container.bind(targetFn, (container) =>
+  targetFn({ dep1: container.resolve("foo") })
+);
 
 describe("Container", () => {
-  describe('#get', () => {
-    it('should be container state', () => {
-      expect(container.deps).toMatchInlineSnapshot()
-    })
-  })
+  describe("#get", () => {
+    it("should be container state", () => {
+      expect(Array.from(container.deps).length).toBe(2);
+    });
+  });
   describe("#resolve", () => {
     describe("when dependencies can not be resolved", () => {
       it("should be resoled", () => {
-        expect(() => { container.resolve("doo") }).toThrowError(
-          "doo is not defined"
-        )
-      })
-    })
+        expect(() => {
+          container.resolve("doo");
+        }).toThrowError("doo is not defined");
+      });
+    });
     describe("when dependencies can be resolved", () => {
       it("should be resoled", () => {
-        const target = container.resolve(targetFn)
-        expect(target()).toBe("foo")
-      })
-    })
-  })
-})
+        const target = container.resolve(targetFn);
+        expect(target()).toBe("foo");
+      });
+    });
+  });
+});
