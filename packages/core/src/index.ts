f5343349d5530d6d0491081d90677dda73bb82a0
himanoa <matsunoappy@gmail.com>
2020-10-05 12:11:44 +0900

    prettier

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,26 +1,30 @@
-type Resolver<T extends {(d: D): T}, D> =  (container: Container) => ReturnType<T>
-type Class<T> = T extends (null | undefined) ? never : { new(): T }
-type ID<T> = Required<Class<T>> | T
+type Resolver<T extends { (d: D): T }, D> = (
+  container: Container
+) => ReturnType<T>;
+type Class<T> = T extends null | undefined ? never : { new (): T };
+type ID<T> = Required<Class<T>> | T;
 
 export default class Container {
-  private dependenciesMap = new Map()
+  private dependenciesMap = new Map();
 
   constructor() {}
 
   get deps() {
-    return this.dependenciesMap.entries()
+    return this.dependenciesMap.entries();
   }
 
-  bind<T extends {(deps: object): ReturnType<T>}>(key: ID<T>, resolver: Resolver<T, ReturnType<T>>) {
-    this.dependenciesMap.set(key, resolver)
+  bind<T extends { (deps: object): ReturnType<T> }>(
+    key: ID<T>,
+    resolver: Resolver<T, ReturnType<T>>
+  ) {
+    this.dependenciesMap.set(key, resolver);
   }
 
   resolve<T extends { (): ReturnType<T> }>(key: ID<T>): ReturnType<T> {
-    const resolver = this.dependenciesMap.get(key)
-    if(resolver) {
-      return resolver(this)
+    const resolver = this.dependenciesMap.get(key);
+    if (resolver) {
+      return resolver(this);
     }
-    throw new Error(`${key} is not defined`)
+    throw new Error(`${key} is not defined`);
   }
 }
-
