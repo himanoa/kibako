d883a010eb877adaf209a7fc048f0a37c7ee018e
himanoa <matsunoappy@gmail.com>
2020-10-16 05:14:42 +0900

    WIP (#12)
    * Initialize react-example package
    
    * "packages/*/example"
    
    * A
    
    * yarn
    
    * Exec `yarn add --dev snowpack`
    
    * WIP
    
    * Add snowpack config
    
    * Add @snowpack/app-scripts-react
    
    * Add snowpack scripts
    
    * WIP
    
    * types
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Add a example
    
    * Remove build
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * WIP
    
    * example
    
    * WIP

---
+++ packages/react/example/src/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Container from "@kibako/core";
+import { useImpl, KibakoContext } from "@kibako/react";
+
+const container = new Container();
+
+function buildFoo(deps: {}) {
+  return () => 114514
+}
+
+function buildBar(deps: {
+  foo: () => number
+}) {
+  return () => deps.foo() + 12
+}
+
+container.bind(buildFoo, () => buildFoo({}))
+container.bind(buildBar, (c) => buildBar({foo: c.resolve(buildFoo)}))
+
+const App = () => {
+  const bar = useImpl(buildFoo)
+
+  return <button onClick={() => console.log(bar())}>Click me</button>
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <KibakoContext.Provider value={container}>
+      <App />
+    </KibakoContext.Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
