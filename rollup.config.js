8dd0937c72f3544b1396dbac7d6520c4b5b61174
himanoa <matsunoappy@gmail.com>
2020-10-16 01:57:33 +0900

    Add rollup config js

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import dts from "rollup-plugin-dts";
+import typescript from 'rollup-plugin-typescript2';
+
+const config = [
+  {
+    input: "./src/index.ts",
+    output: [{ file: "lib/index-esm.js", format: "es" }],
+    plugins: [typescript()],
+  },
+  {
+    input: "./src/index.ts",
+    output: [{ file: "lib/index-cjs.js", format: "cjs" }],
+    plugins: [typescript()],
+  },
+  {
+    input: "./lib/index.d.ts",
+    output: [{ file: "./types/index.d.ts" }],
+    plugins: [dts()],
+  },
+];
+
+export default config;
