ec5cac85d3d44f403507fe9b7f593ad53ebc2367
himanoa <matsunoappy@gmail.com>
2020-10-16 03:41:22 +0900

    Implemented useImpl

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,10 +3,12 @@ import Container from '@kibako/core';
 
 export const KibakoContext = createContext<Container | null>(null)
 
-export const useContainer = () => {
+export const useImpl = <T extends {(deps: object): ReturnType<T>}>(key: T): ReturnType<T> => {
   const container = useContext(KibakoContext)
 
   if(container === null) {
     throw new Error("container is not provided")
   }
+
+  return container.resolve(key)
 }
