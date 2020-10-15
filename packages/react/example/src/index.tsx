d6c6a467f65203569fa3a392efa3111c96239283
himanoa <matsunoappy@gmail.com>
2020-10-16 04:36:05 +0900

    Add a example

--- packages/react/example/src/index.tsx
+++ packages/react/example/src/index.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Container from "@kibako/core";
+import { useImpl, KibakoContext } from "@kibako/react";
+
+const container = new Container();
+
+function buildFoo(deps: {}) {
+  return () => 114514
+}
+
+function buildBar(deps: {
+  foo: () => number
+}) {
+  return () => deps.foo() + 12
+}
+
+container.bind(buildFoo, () => buildFoo({}))
+container.bind(buildBar, (c) => buildBar(c.resolve({deps: buildFoo})))
+
+const App = () => {
+  const bar = useImpl(buildBar)
+
+  return <button onClick={bar}>Click me</button>
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <KibakoContext.Provider value={container}>
+      <App />
+    </KibakoContext.Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
-
-if (import.meta.hot) {
-  import.meta.hot.accept();
-}
